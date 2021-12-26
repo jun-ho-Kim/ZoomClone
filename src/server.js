19a2c0829e001dfad3ff81e22718372a6654aab4
jun-ho-Kim <wnsgh5049@naver.com>
2021-12-27 08:12:19 +0900

    WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import http from 'http';
+import WebSocket from 'ws';
+
 
 const app = express();
 
-app.set("view engine", "pug")
-app.set("views", __dirname + "/views")
-app.use("/public", express.static(__dirname + "/public"))
-app.get("/", (req, res) => res.render("home"))
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+app.use('/public', express.static(__dirname + '/public'))
+app.get('/', (req, res) => res.render('home'))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 
-app.listen(4000, handleListen);
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+function handleConnection(socket) {
+    console.log('socket', socket)
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(3000, handleListen);
