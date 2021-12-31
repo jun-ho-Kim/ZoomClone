919d905c3beb62d1f102b725a969aef2e835dbd7
jun-ho-Kim <wnsgh5049@naver.com>
2021-12-31 15:32:33 +0900

    Message send parse JSON

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,12 +1,21 @@
 const messageList = document.querySelector('ul')
-const messageForm = document.querySelector('form')
+const nickForm = document.querySelector('#nick')
+const messageForm = document.querySelector('#message')
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+function makeMessage(type, payload) {
+    const msg = { type, payload }
+    return JSON.stringify(msg)
+}
 
 socket.addEventListener('open', () => {
     console.log('Connected to Server ✅')
 })
 
 socket.addEventListener('message', (message) => {
+    const li = document.createElement('li')
+    li.innerText = message.data
+    messageList.append(li)
     console.log('New Message', message)
 })
 
@@ -16,9 +25,17 @@ socket.addEventListener('close', () => {
 
 function handleSumit(event) {
     event.preventDefault()
-    const input = messageForm.querySelector("input")
-    socket.send(input.value)
+    const input = messageForm.querySelector('input')
+    socket.send(makeMessage('new_message', input.value));
+    input.value = ''
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault()
+    const input = nickForm.querySelector('input')
+    socket.send(makeMessage('nickname', input.value))
     input.value = ''
 }
 
 messageForm.addEventListener('submit', handleSumit)
+nickForm.addEventListener('submit', handleNickSubmit)
