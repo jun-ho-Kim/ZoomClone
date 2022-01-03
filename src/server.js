a0b2acad3292f8e042f866ba0a1ce544dc5ec67d
jun-ho-Kim <wnsgh5049@naver.com>
2022-01-03 09:01:21 +0900

    [SOCKETIO] weclome meesage
    1. socket.to('room name').emit('event name')은 본인을 제외한 클라이언트에게 이벤트를 설정한다.
    
    2. frontend에서 event에 반응하도록 만들기 위해서 socket.on()을 사용하여 이벤트를 실행

--- src/server.js
+++ src/server.js
@@ -13,12 +13,13 @@ const httpServer = http.createServer(app)
 const wsServer = new Server(httpServer)
 
 wsServer.on('connection', (socket) => {
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`)
+    })
     socket.on('enter_room', (roomName, done) => {
-        socket.onAny((event) => {
-            console.log(`Socket Event: ${event}`)
-        })
         socket.join(roomName)
         done()
+        socket.to(roomName).emit('welcome')
     })
 })
 
