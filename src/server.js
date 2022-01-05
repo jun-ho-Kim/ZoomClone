5c4d0b7f7da4169e76e8f6df2601c865c95cc4be
jun-ho-Kim <wnsgh5049@naver.com>
2022-01-06 08:42:45 +0900

    [SOCKETIO] Adapter
    - 어댑터가 기본적으로 하는 일은 다른 서버들 사이에 실시간 어플리케이션을 동기화 하는 것이다.
    - 클라이언트가 서로 다른 두 서버 간의 메세지를 보내고 싶다면 Adapter를 사용하면  된다.

--- src/server.js
+++ src/server.js
@@ -13,9 +13,28 @@ app.get('/', (req, res) => res.render('home'))
 const httpServer = http.createServer(app)
 const wsServer = new Server(httpServer)
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms }
+        }
+    } = wsServer
+
+    const publicRooms = []
+
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key)
+        }
+    })
+
+    return publicRooms
+}
+
 wsServer.on('connection', (socket) => {
     socket["nickname"] = "Anon"
     socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter)
         console.log(`Socket Event: ${event}`)
     })
     socket.on('enter_room', (roomName, done) => {
