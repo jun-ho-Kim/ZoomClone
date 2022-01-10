6f95e34a608a510188b998405a8e67d1a393d18d
jun-ho-Kim <wnsgh5049@naver.com>
2022-01-11 08:36:58 +0900

    [VIDEO CALL] Call Controls
    - 비디오 트랙에 접근(myStream.getVideoTracks() 사용해서 가져오기)
    - camera 장치 가져와서 html에 표시하기
    (navigator.mediaDevices.enumerateDevices()) 사용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,26 @@ const socket = io()
 const myFace = document.getElementById('myFace')
 const muteBtn = document.getElementById('mute')
 const cameraBtn = document.getElementById('camera')
+const camerasSelecet = document.getElementById('cameras')
 
 let myStream
 let muted = false
 let cameraOff = false
+
+async function getCameras() {
+    try {
+        const devices = await this.navigator.mediaDevices.enumerateDevices()
+        const cameras = devices.filter(device => device.kind === "videoinput")
+        cameras.forEach((camera) => {
+            const option = document.createElement('option')
+            option.value = camera.deviceId
+            option.innerText = camera.label
+            camerasSelecet.appendChild(option)
+        })
+    } catch (error) {
+        console.log('error', error)
+    }
+}
 
 async function getMedia() {
     try {
@@ -14,7 +30,7 @@ async function getMedia() {
             audio: false,
             video: true,
         })
-
+        getCameras()
         myFace.srcObject = myStream
     } catch (error) {
         console.log('error', error)
@@ -34,6 +50,9 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+    myStream
+        .getVideoTracks()
+        .forEach((track) => track.enabled = !track.enabled)
     if (cameraOff) {
         cameraBtn.innerText = 'Turn Camera off'
         cameraOff = false
